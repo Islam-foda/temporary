d4ae2138af834d67576560fd7b73603ea0ad4023
islam-foda <islam@alex-support.juhaynafood.ind>
2024-07-13 12:41:38 +0300

    update

--- test.js
+++ test.js
@@ -5,12 +5,12 @@ const server = http.createServer((req, res) => {
     res.write("welcome to our website");
     res.end();
   } else if (req.url === "/about") {
-    res.write("This is a berief for the website");
+    res.write("This is a berief for the content of the website");
     res.end();
   } else
     res.write(`
     <h1>OOPS!</h1>
-    <p>the page doesn't exist</p>
+    <p>the page you visit does not exist</p>
     <a href="/">go Home</a>
     `);
   res.end();
